80ad9b387adf35c529f248ca9ea0c290fec3b410
wq.chu <wq.chu@tianrang-inc.com>
2021-02-18 21:36:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+### DeepL script
+
+- 英译中：deepl-en-zh.sh
+- 中译英：deepl-zh-en.sh
+
+![readme-images](./readme-images/deepl.png)
---
+++ deepl-en-zh.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Dependency: requires curl (https://curl.se/)
+# Install via homebrew: `brew install curl`
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title DeepL-EN-ZH
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.argument1 { "type": "text", "placeholder": "Text" }
+
+curl -s --request POST 'https://www2.deepl.com/jsonrpc' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "id": 90750013,
+    "jsonrpc": "2.0",
+    "method": "LMT_handle_jobs",
+    "params": {
+        "commonJobParams": {},
+        "jobs": [
+            {
+                "kind": "default",
+                "preferred_num_beams": 1,
+                "quality": "fast",
+                "raw_en_context_after": [],
+                "raw_en_context_before": [],
+                "raw_en_sentence": '"\"$1\""'
+            }
+        ],
+        "lang": {
+            "source_lang_user_selected": "EN",
+            "target_lang": "ZH",
+            "user_preferred_langs": [
+                "ZH",
+                "EN"
+            ]
+        },
+        "priority": -1,
+        "timestamp": 1613644286977
+    }
+}
+' | \
+python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["result"]["translations"][0]["beams"][0]["postprocessed_sentence"]);'
